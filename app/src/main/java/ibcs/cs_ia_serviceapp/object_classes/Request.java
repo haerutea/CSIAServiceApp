1bf47b43c53b045558473272938cd37e7834b9c7
haerutea <tszya2020@student.cis.edu.hk>
2019-09-12 10:23:42 +0800

    added getters and setters

--- app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Request.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Request.java
@@ -1,28 +1,89 @@
 package ibcs.cs_ia_serviceapp.object_classes;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Request {
+public class Request implements Serializable {
 
     private String language;
     private String service;
     private String priority;
     private String location;
     private String description;
-    private ArrayList<String> filenames;
+    private String filename;
 
     public Request()
     {
 
     }
 
-    public Request(String inLang, String inServ, String inPri, String inLoc, String inDesc, ArrayList<String> inNames)
+    public Request(String inLang, String inServ, String inPri, String inLoc, String inDesc, String inNames)
     {
         language = inLang;
         service = inServ;
         priority = inPri;
         location = inLoc;
         description = inDesc;
-        filenames = inNames;
+        filename = inNames;
+    }
+
+    public String getLanguage()
+    {
+        return language;
+    }
+
+    public String getService()
+    {
+        return service;
+    }
+
+    public String getPriority()
+    {
+        return priority;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getFilename()
+    {
+        return filename;
+    }
+
+    public void setLanguage(String inLang)
+    {
+        this.language = inLang;
+    }
+
+    public void setService(String inService)
+    {
+        this.service = inService;
+    }
+
+    public void setPriority(String inPriority)
+    {
+        this.priority = inPriority;
+    }
+
+    public void setLocation(String inLocation)
+    {
+        this.location = inLocation;
+    }
+
+    public void setDescription(String inDesc)
+    {
+        this.description = inDesc;
+    }
+
+    public void setFilename(String inFilename)
+    {
+        this.filename = inFilename;
     }
 }

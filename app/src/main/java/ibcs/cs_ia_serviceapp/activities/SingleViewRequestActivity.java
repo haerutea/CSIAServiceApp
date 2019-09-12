c53ef33b519cf789cec85267d0a2258f2bd0c351
haerutea <tszya2020@student.cis.edu.hk>
2019-09-12 15:50:49 +0800

    download image from firebase works

---
+++ app/src/main/java/ibcs/cs_ia_serviceapp/activities/SingleViewRequestActivity.java
@@ -0,0 +1,61 @@
+package ibcs.cs_ia_serviceapp.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import ibcs.cs_ia_serviceapp.R;
+import ibcs.cs_ia_serviceapp.object_classes.Request;
+import ibcs.cs_ia_serviceapp.utils.Constants;
+
+public class SingleViewRequestActivity extends AppCompatActivity {
+
+    //UI
+    private ImageView imageView;
+
+    //Fields
+    private String uid;
+    private Uri imageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_view_requests_activity);
+
+        uid = "hi";
+        imageView = findViewById(R.id.viewall_img_view);
+
+        Constants.REQUEST_REFERENCE.child(uid).addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                Request req = dataSnapshot.getValue(Request.class);
+                if(req != null)
+                    Constants.STORAGE_REFERENCE.child(uid).child(req.getFilename()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imageUrl = uri;
+                            Glide.with(SingleViewRequestActivity.this).load(uri).into(imageView);
+                        }
+                    });
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}

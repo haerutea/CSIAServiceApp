b2ca9a3bea29310199668f5c8231be75612d2153
haerutea <tszya2020@student.cis.edu.hk>
2019-09-26 14:47:47 +0800

    deleted receiverId since RequestObj already has a field for it

--- app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Quota.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Quota.java
@@ -4,7 +4,6 @@ public class Quota
 {
     public int price;
     public String providerUid;
-    public String receiverUid;
     public Request requestObj;
 
     public Quota()
@@ -15,7 +14,6 @@ public class Quota
     {
         price = inPrice;
         providerUid = inProviderUid;
-        receiverUid = inReceiverUid;
         requestObj = inRequest;
     }
 
@@ -27,11 +25,6 @@ public class Quota
     public String getProviderUid()
     {
         return providerUid;
-    }
-
-    public String getReceiverUid()
-    {
-        return receiverUid;
     }
 
     public Request getRequestObj()
@@ -48,12 +41,7 @@ public class Quota
     {
         this.providerUid = providerUid;
     }
-
-    public void setReceiverUid(String receiverUid)
-    {
-        this.receiverUid = receiverUid;
-    }
-
+    
     public void setRequestObj(Request requestObj)
     {
         this.requestObj = requestObj;

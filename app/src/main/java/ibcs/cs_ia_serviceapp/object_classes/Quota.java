9364dc1bc5d50e1bd2532fb5a447e65b071ce8b6
haerutea <tszya2020@student.cis.edu.hk>
2019-09-29 20:06:19 +0800

    edited to fit database structure

--- app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Quota.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/object_classes/Quota.java
@@ -1,20 +1,23 @@
 package ibcs.cs_ia_serviceapp.object_classes;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Quota implements Serializable
 {
     public int price;
-    public String providerUid;
+    public HashMap<String, Boolean> providerUid;
 
     public Quota()
     {
+        providerUid = new HashMap<>();
     }
 
-    public Quota(int inPrice, String inProviderUid)
+    public Quota(int inPrice, HashMap<String, Boolean> inProviderUid)
     {
         price = inPrice;
         providerUid = inProviderUid;
+        //providerUid = "";
     }
 
     public int getPrice()
@@ -22,18 +25,28 @@ public class Quota implements Serializable
         return price;
     }
 
+    public HashMap<String, Boolean> getProviderUid() {
+        return providerUid;
+    }
+
+    /*
     public String getProviderUid()
     {
         return providerUid;
-    }
+    }*/
 
     public void setPrice(int price)
     {
         this.price = price;
     }
 
+    public void setProviderUid(HashMap<String, Boolean> providerUid) {
+        this.providerUid = providerUid;
+    }
+
+    /*
     public void setProviderUid(String inproviderUid)
     {
         this.providerUid = inproviderUid;
-    }
+    }*/
 }

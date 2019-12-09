df3c02d2cfe90c9f80dfc33b25414510b64508ef
haerutea <tszya2020@student.cis.edu.hk>
2019-12-09 09:00:39 +0800

    added single request activity

---
+++ app/src/main/java/ibcs/cs_ia_serviceapp/activities/CustomerSingleRequestActivity.java
@@ -0,0 +1,87 @@
+package ibcs.cs_ia_serviceapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+
+import ibcs.cs_ia_serviceapp.R;
+import ibcs.cs_ia_serviceapp.object_classes.Quota;
+import ibcs.cs_ia_serviceapp.object_classes.Request;
+import ibcs.cs_ia_serviceapp.utils.Constants;
+import ibcs.cs_ia_serviceapp.utils.QuotaAdapter;
+import ibcs.cs_ia_serviceapp.utils.UserSharedPreferences;
+
+public class CustomerSingleRequestActivity extends AppCompatActivity
+{
+    //UI
+    private TextView requestView;
+    private TextView languageView;
+    private TextView serviceView;
+    private TextView priorityView;
+    private TextView locationView;
+    private TextView descriptionView;
+    private ImageView imageView;
+
+    //Fields
+    private String uid;
+    private Request inRequest;
+    private LinearLayoutManager linearLayoutManager;
+    private QuotaAdapter adapter;
+    private RecyclerView quotasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_single_request_activity);
+
+        inRequest = (Request) getIntent().getSerializableExtra(Constants.REQUEST_KEY);
+        uid = UserSharedPreferences.getInstance(CustomerSingleRequestActivity.this).getStringInfo(Constants.UID_KEY);
+        if(inRequest != null)
+        {
+            requestView = findViewById(R.id.request_title_view);
+            languageView = findViewById(R.id.language_title);
+            serviceView = findViewById(R.id.service_title);
+            priorityView = findViewById(R.id.priority_title);
+            locationView = findViewById(R.id.location_title);
+            descriptionView = findViewById(R.id.description);
+            imageView = findViewById(R.id.viewall_img_view);
+
+            requestView.setText(getString(R.string.title_format, inRequest.getTitle()));
+            languageView.setText(getString(R.string.lang_format, inRequest.getLanguage()));
+            serviceView.setText(getString(R.string.service_format, inRequest.getService()));
+            priorityView.setText(getString(R.string.priority_format, inRequest.getPriority()));
+            locationView.setText(getString(R.string.location_format, inRequest.getLocation()));
+            descriptionView.setText(inRequest.getDescription());
+            //https://stackoverflow.com/questions/50816557/storing-and-displaying-image-using-glide-firebase-android
+            Constants.STORAGE_REFERENCE.child(inRequest.getSubmitterUid()).child(inRequest.getFilename())
+                    .getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+            {
+                //https://github.com/bumptech/glide#how-do-i-use-glide
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(CustomerSingleRequestActivity.this).load(uri).into(imageView);
+                }
+            });
+
+            linearLayoutManager = new LinearLayoutManager(CustomerSingleRequestActivity.this);
+            ArrayList<Quota> quotas = inRequest.getQuotas();
+            quotas.remove(0);
+            adapter = new QuotaAdapter(quotas);
+            quotasView = findViewById(R.id.all_quotas_recycler);
+            quotasView.setLayoutManager(linearLayoutManager);
+            quotasView.setAdapter(adapter);
+        }
+    }
+
+}

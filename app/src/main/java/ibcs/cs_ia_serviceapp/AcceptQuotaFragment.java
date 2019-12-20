c816af6089d7cf1b769d2cd691e4eafa314f76d3
haerutea <tszya2020@student.cis.edu.hk>
2019-12-20 22:14:24 +0800

    added rid, changed textView to use string formatting, fixed acceptQuota to fit database structure

--- app/src/main/java/ibcs/cs_ia_serviceapp/AcceptQuotaFragment.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/AcceptQuotaFragment.java
@@ -1,20 +1,23 @@
 package ibcs.cs_ia_serviceapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 
-import java.util.ArrayList;
+import com.google.firebase.database.DatabaseReference;
+
 import java.util.HashMap;
 
+import ibcs.cs_ia_serviceapp.activities.ChatActivity;
+import ibcs.cs_ia_serviceapp.object_classes.Chat;
 import ibcs.cs_ia_serviceapp.object_classes.Quota;
 import ibcs.cs_ia_serviceapp.object_classes.Request;
 import ibcs.cs_ia_serviceapp.utils.Constants;
@@ -22,19 +25,19 @@ import ibcs.cs_ia_serviceapp.utils.UserSharedPreferences;
 
 
 /**
- * fragment shown when the service provider wants to press on send quota
+ * fragment shown when the submitter wants to accept or reject quota
  */
 public class AcceptQuotaFragment extends DialogFragment implements View.OnClickListener {
     //UI
-    private EditText pricingField;
+    private TextView pricingField;
     private Button bReject;
     private Button bAccept;
 
     //Fields
     private Request requestObj;
+    private String requestId;
     private Quota quotaObj;
     private String userUid;
-    private int price;
 
     public AcceptQuotaFragment()
     {
@@ -46,11 +49,12 @@ public class AcceptQuotaFragment extends DialogFragment implements View.OnClickL
      * @param inQuota quota object user is trying to accept or reject
      * @return A new instance of fragment SendQuotaFragment.
      */
-    public static AcceptQuotaFragment newInstance(Quota inQuota)
+    public static AcceptQuotaFragment newInstance(Quota inQuota, Request inRequest)
     {
         AcceptQuotaFragment fragment = new AcceptQuotaFragment();
         Bundle args = new Bundle();
         args.putSerializable(Constants.QUOTA_KEY, inQuota);
+        args.putSerializable(Constants.REQUEST_KEY, inRequest);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,17 +66,21 @@ public class AcceptQuotaFragment extends DialogFragment implements View.OnClickL
         userUid = UserSharedPreferences.getInstance(getContext()).getStringInfo(Constants.UID_KEY);
         if (getArguments() != null) {
             quotaObj = (Quota) getArguments().getSerializable(Constants.QUOTA_KEY);
+            requestObj = (Request) getArguments().getSerializable(Constants.REQUEST_KEY);
+            requestId = requestObj.getRid();
         }
+        userUid = UserSharedPreferences.getInstance(getContext()).getStringInfo(Constants.UID_KEY);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View baseView = inflater.inflate(R.layout.send_quota_fragment, container, false);
-        pricingField = baseView.findViewById(R.id.send_pricing_field);
-        bReject = baseView.findViewById(R.id.accept_quota_reject_button);
-        bAccept = baseView.findViewById(R.id.accept_quota_accept_button);
+        View baseView = inflater.inflate(R.layout.accept_quota_fragment, container, false);
+        pricingField = baseView.findViewById(R.id.textView_show_pricing);
+        pricingField.setText(getString(R.string.show_price_format, quotaObj.getPrice()));
+        bReject = baseView.findViewById(R.id.button_accept_quota_reject);
+        bAccept = baseView.findViewById(R.id.button_accept_quota_accept);
 
         bReject.setOnClickListener(this);
         bAccept.setOnClickListener(this);
@@ -81,19 +89,46 @@ public class AcceptQuotaFragment extends DialogFragment implements View.OnClickL
 
     private void acceptQuota()
     {
-        Constants.REQUEST_REFERENCE.child(requestObj.getSubmitterUid())
-                .child(requestObj.getTitle()).child(Constants.ACCEPTED_KEY).setValue(true);
-        Toast.makeText(getContext(), "Your quota has been sent.", Toast.LENGTH_SHORT).show();
+        DatabaseReference currentRequestRef = Constants.REQUEST_REFERENCE.child(requestId);
+        //delete all quotas but the accepted one, change quota to accepted
+        HashMap<String, Quota> quotas = new HashMap<>();
+        quotaObj.setAccepted(true);
+        quotas.put(quotaObj.getQuotaId(), quotaObj);
+        currentRequestRef.child(Constants.QUOTA_PATH).setValue(quotas);
+
+        //update request info: set accepted to true, update providerUid
+        currentRequestRef.child(Constants.ACCEPTED_KEY).setValue(true);
+        currentRequestRef.child(Constants.PROVIDER_UID_KEY).setValue(quotaObj.getProviderUid());
+
+        //remove requestId from submitted and move it to ongoing
+        Constants.USER_REFERENCE.child(userUid).child(Constants.REQUESTS_SUBMITTED_PATH).child(requestId).removeValue();
+        Constants.USER_REFERENCE.child(userUid).child(Constants.REQUESTS_ONGOING_PATH).child(requestId).setValue(true);
+
+        //move requestId to ongoing for provider too
+        Constants.USER_REFERENCE.child(quotaObj.getProviderUid())
+                .child(Constants.REQUESTS_ONGOING_PATH).child(requestId).setValue(true);
+
+        //make chat object and start chatActivity
+        Toast.makeText(getContext(), "Quota has been accepted.", Toast.LENGTH_SHORT).show();
+        Chat newChatlog = new Chat(userUid, quotaObj.getProviderUid());
+        currentRequestRef.child(Constants.CHAT_PATH).setValue(newChatlog);
+        Intent intent = new Intent(getContext(), ChatActivity.class);
+        intent.putExtra(Constants.RID_KEY, requestObj.getRid());
+        startActivity(intent);
         dismiss();
+
     }
 
     private void rejectQuota()
     {
-        ArrayList<Quota> tempList = requestObj.getQuotas();
-        tempList.remove(quotaObj);
-        Constants.REQUEST_REFERENCE.child(requestObj.getSubmitterUid())
-                .child(requestObj.getTitle()).child(Constants.QUOTA_PATH).setValue(tempList);
-        Toast.makeText(getContext(), "The quota has been rejected", Toast.LENGTH_SHORT).show();
+        HashMap<String, Quota> tempMap = requestObj.getQuotas();
+        tempMap.remove(quotaObj.getQuotaId());
+        Constants.REQUEST_REFERENCE.child(requestObj.getRid()).child(Constants.QUOTA_PATH).setValue(tempMap);
+
+        //remove quotaId from submitted for provider
+        Constants.USER_REFERENCE.child(quotaObj.getProviderUid())
+                .child(Constants.QUOTAS_SUBMITTED_PATH).child(quotaObj.getQuotaId()).removeValue();
+        Toast.makeText(getContext(), "Quota has been rejected.", Toast.LENGTH_SHORT).show();
         dismiss();
     }
 
@@ -105,7 +140,7 @@ public class AcceptQuotaFragment extends DialogFragment implements View.OnClickL
         {
             acceptQuota();
         }
-        else if( i == bReject.getId())
+        else if(i == bReject.getId())
         {
             rejectQuota();
         }

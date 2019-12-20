d6b73de5e3090c777bec647b6f4bbac9f7d758b3
haerutea <tszya2020@student.cis.edu.hk>
2019-12-20 22:11:20 +0800

    added strings for requests submitted/ongoing/completed, rid, submit/provider uid, completed bool path, dummy string

--- app/src/main/java/ibcs/cs_ia_serviceapp/utils/Constants.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/utils/Constants.java
@@ -41,28 +41,35 @@ public class Constants {
     public static final String CHAT_PATH = "chats";
     public static final String MESSAGE_PATH = "messages";
     public static final String REQUEST_PATH = "requests";
-    public static final String SUBMITTED_PATH = "submitted";
-    public static final String ONGOING_PATH = "ongoing";
-    public static final String COMPLETED_PATH = "completed";
+    public static final String REQUESTS_SUBMITTED_PATH = "requestsSubmitted";
+    public static final String QUOTAS_SUBMITTED_PATH = "quotasSubmitted";
+    public static final String REQUESTS_ONGOING_PATH = "requestsOngoing";
+    public static final String REQUESTS_COMPLETED_PATH = "completed";
     public static final String QUOTA_PATH = "quotas";
     public static final String TOKEN_KEY = "token";
     public static final String UID_KEY = "uid";
+    public static final String RID_KEY = "rid";
+    public static final String SUBMITTER_UID_KEY = "submitterUid";
+    public static final String PROVIDER_UID_KEY = "providerUid";
     public static final String USERNAME_KEY = "username";
     public static final String ACCOUNT_TYPE_KEY = "accountType";
     public static final String ONLINE_KEY = "online";
     public static final String REQUEST_KEY = "request";
     public static final String QUOTA_KEY = "quota";
     public static final String ACCEPTED_KEY = "accepted";
-    public static final String CHAT_ROOM_ID_KEY = "chatRoomID";
+    public static final String COMPLETED_CUSTOMER = "completedCustomer";
+    public static final String COMPLETED_PROVIDER = "completedProvider";
+    public static final String DUMMY_STRING = "a1bc2s2364bds579t5ga3cg5bdkjhds";
     public static final DatabaseReference BASE_REFERENCE = FirebaseDatabase.getInstance().getReference();
     public static final DatabaseReference USER_REFERENCE = BASE_REFERENCE.child(USER_PATH);
     public static final DatabaseReference REQUEST_REFERENCE = BASE_REFERENCE.child(REQUEST_PATH);
-    public static final DatabaseReference CHAT_REFERENCE = BASE_REFERENCE.child(CHAT_PATH);
     public static final StorageReference STORAGE_REFERENCE = FirebaseStorage.getInstance().getReference();
+
 
     //for notifications
     public static final String CHANNEL_ID = "ServiceChatID";
     public static final String CHANNEL_NAME = "ServiceChat";
+    public static final String NOTIF_TITLE = "You have a new quota request!";
     public static final String CHANNEL_DES = "temp";
     public static final int CHAT_NOTIF_ID = 1;
 }

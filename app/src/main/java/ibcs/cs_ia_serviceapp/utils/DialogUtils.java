a71d7f23b0f696096aea93abfa8c6733a32a959f
haerutea <tszya2020@student.cis.edu.hk>
2020-01-14 23:58:15 +0800

    got rid of useless method

--- app/src/main/java/ibcs/cs_ia_serviceapp/utils/DialogUtils.java
+++ app/src/main/java/ibcs/cs_ia_serviceapp/utils/DialogUtils.java
@@ -1,16 +1,7 @@
 package ibcs.cs_ia_serviceapp.utils;
 
-import android.app.AlertDialog;
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 /**
  * utils class to create and show ProgressDialogs easier
@@ -19,71 +10,20 @@ public class DialogUtils
 {
 
     //https://stackoverflow.com/a/37428936
-
     /**
      * creates and shows new progress dialog, then returns the object so it can be dismissed.
      *
-     * @param context context of where it'll show up
+     * @param activity activity of where it'll show up
      * @param message message to be shown on dialog
      * @return the created ProgressDialog object so it can be dismissed
      */
-    public static ProgressDialog showProgressDialog(Context context, String message)
+    public static ProgressDialog makeProgressDialog(Activity activity, String message)
     {
-        ProgressDialog progress = new ProgressDialog(context);
+        ProgressDialog progress = new ProgressDialog(activity);
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progress.setMessage(message);
         progress.setIndeterminate(true);
         progress.setCancelable(false);
-        progress.show();
         return progress;
-    }
-
-    //https://stackoverflow.com/a/49272722
-    public static AlertDialog makeDialog(Context context, String message)
-    {
-        int llPadding = 30;
-        LinearLayout ll = new LinearLayout(context);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
-        ll.setPadding(llPadding, llPadding, llPadding, llPadding);
-        ll.setGravity(Gravity.CENTER);
-        LinearLayout.LayoutParams llParam = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        llParam.gravity = Gravity.CENTER;
-        ll.setLayoutParams(llParam);
-
-        ProgressBar progressBar = new ProgressBar(context);
-        progressBar.setIndeterminate(true);
-        progressBar.setPadding(0, 0, llPadding, 0);
-        progressBar.setLayoutParams(llParam);
-
-        llParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        llParam.gravity = Gravity.CENTER;
-        TextView tvText = new TextView(context);
-        tvText.setText(message);
-        tvText.setTextColor(Color.parseColor("#000000"));
-        tvText.setTextSize(20);
-        tvText.setLayoutParams(llParam);
-
-        ll.addView(progressBar);
-        ll.addView(tvText);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setCancelable(true);
-        builder.setView(ll);
-
-        AlertDialog dialog = builder.create();
-        //dialog.show();
-        Window window = dialog.getWindow();
-        if (window != null)
-        {
-            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-            layoutParams.copyFrom(dialog.getWindow().getAttributes());
-            layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT;
-            layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT;
-            dialog.getWindow().setAttributes(layoutParams);
-        }
-
-        return dialog;
     }
 }
